ff2a697eaf876a2bacc7d6ee59ffe5a1777b9293
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2024-10-15 01:47:07 +0100

    Initial Commit

---
+++ AppMusica/AppMusicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMusicaApp.swift
+//  AppMusica
+//
+//  Created by Ryan Carlos Da costa on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppMusicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
